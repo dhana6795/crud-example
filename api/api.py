9c05b68ff910fb3dce33136c421aede4114e249f
dhanalakshmi <dhanalakshmi@localhost.localdomain>
2020-05-02 22:51:33 +0530

    Added Code for basic crud app.

---
+++ api/api.py
@@ -0,0 +1,36 @@
+import time
+from flask import Flask
+from flask import request
+import sqlalchemy
+import fdb
+from sqlalchemy import create_engine
+import json
+
+app = Flask(__name__)
+engine = create_engine('firebird+fdb://SYSDBA:masterkey@localhost:3050//var/lib/firebird/2.5/data/Test.gdb')
+engine.connect()
+session = engine
+@app.route('/get')
+def get_employees():
+    emp =  session.execute("select * from Employee")
+    emp_list = []
+    for row in emp:
+        emp_list.append({'id': row['id'], 'name': row['name'], 'designation': row['desgnation'], 'image': row['image_url']})
+    return {'emp': json.dumps(emp_list)}
+
+@app.route('/save', methods=["POST"])
+def save():
+    session.execute("insert into Employee (name, desgnation, image_url) values ('"+ request.form.get('name')+"','"+request.form.get('desg')+ "','"+request.form.get('imageUrl') +"')")
+    
+    return {'status':'record created successfully!!!'}
+
+@app.route('/update', methods=["POST"])
+def update():
+    session.execute("UPDATE Employee set name = '" + request.json.get('name') + "'," + "desgnation = '" + request.json.get('designation')+ "', image_url = '" + request.json.get('image')+ "' where id ="+str(request.json.get('id')))    
+    return {'status':'record updated successfully!!!'}
+
+
+@app.route('/delete', methods=["DELETE"])
+def delete():
+    session.execute("delete from Employee where id ="+ str(request.args.get('id')))
+    return {'status':'record deleted.'}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,227 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Header, Image, Table } from 'semantic-ui-react'
+import { Form, Input, Button, Icon } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    fetch('/get').then(res => res.json()).then(data => {
+      data = JSON.parse(data.emp);
+      setData(data);
+    });
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>Example CRUD page</p>
+      </header><br/>
+      <div className="ui container">
+        <div class="ui divided two column grid">
+          <div class="column">
+            <TableEmployee data={data} />
+          </div>
+          <div class="column">
+            <EmpForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+export class EmpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.state = { name: '', desg: '', imageUrl: '' };
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Form action="/save" method="post" >
+
+          <Form.Field
+            id='form-input-control-emp-name'
+            control={Input}
+            label='Employee Name'
+            placeholder='Employee Name'
+            value={this.state.name}
+            onChange={this.onChange}
+            name="name"
+          />
+
+          <Form.Field
+            id='form-input-control-emp-desg'
+            control={Input}
+            label='Designation'
+            placeholder='Designation'
+            value={this.state.desg}
+            onChange={this.onChange}
+            name="desg"
+          />
+          <Form.Field
+            id='form-input-control-img-url'
+            control={Input}
+            label='Image URL'
+            placeholder='Image url'
+            value={this.state.imageUrl}
+            onChange={this.onChange}
+            name="imageUrl"
+          />
+          <Button type='submit'>Submit</Button>
+
+        </Form>
+      </div >
+    );
+  }
+}
+
+export class TableEmployee extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { editingElement: 0 };
+    this.onEdit = this.onEdit.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onEditConfirm = this.onEditConfirm.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onEdit(e) {
+    let id = e.target.getAttribute('id');
+    let data = this.props.data.find(x => x.id == e.target.getAttribute('id'));
+    this.setState({ editingElement: id, name: data.name, designation: data.designation, image: data.image });
+  }
+  delete(id) {
+    return fetch('/delete?id=' + id, {
+      method: 'delete'
+    }).then(response => response.json());
+  }
+  onDelete(e) {
+    let id = e.target.getAttribute('id');
+    this.delete(id).then(data => { alert(data['status']) });
+  }
+  onEditConfirm(e) {
+    let id = e.target.getAttribute('id');
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 'id': id, 'name': this.state.name, 'designation': this.state.designation, 'image': this.state.image })
+    };
+
+    (async () => {
+      const rawResponse = await fetch('/update', requestOptions);
+      const content = await rawResponse.json();
+
+      alert(content['status']);
+    })();
+    this.setState({ editingElement: 0 });
+  }
+  onCancel() {
+    this.setState({ editingElement: 0 });
+  }
+  createRow(data) {
+
+    return Object.keys(data).map(key => {
+
+      if (!(this.state.editingElement.toString() === data[key]['id'].toString())) {
+        return (<EmpRow data={data[key]} onEdit={this.onEdit} onDelete={this.onDelete} />)
+
+      } else {
+        return (<EmpRowEdit data={data[key]} onEdit={this.onEditConfirm} onChange={this.onChange} onCancel={this.onCancel} {...this.state} />)
+
+      }
+    })
+
+  }
+  render() {                                                                                                                                
+    return (
+
+      <Table basic='very' padded  collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Employee</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {this.createRow(this.props.data)}
+
+        </Table.Body>
+      </Table>
+    );
+  }
+}
+
+const EmpRowEdit = props => <Table.Row>
+  <Table.Cell>
+    <Form.Field
+      id='form-input-control-emp-name'
+      control={Input}
+      placeholder='Employee Name'
+      value={props.name}
+      onChange={props.onChange}
+      name="name"
+    />
+
+    <Form.Field
+      id='form-input-control-emp-desg'
+      control={Input}
+      placeholder='Designation'
+      value={props.designation}
+      onChange={props.onChange}
+      name="designation"
+    />
+    <Form.Field
+      id='form-input-control-img-url'
+      control={Input}
+      placeholder='Image url'
+      value={props.image}
+      onChange={props.onChange}
+      name="image"
+    />
+  </Table.Cell>
+  <Table.Cell>
+    <Icon color='green' name=' check circle ' link id={props.data.id} onClick={props.onEdit} /> <span>&nbsp;&nbsp;</span>
+    <Icon color='red' name='close icon' link onClick={props.onCancel} id={props.data.id} />
+  </Table.Cell>
+</Table.Row>
+const EmpRow = props => <Table.Row>
+  <Table.Cell>
+    <Header as='h4' image>
+      <Image src={props.data.image} rounded size='mini' />
+      <Header.Content>
+        {props.data.name}
+        <Header.Subheader>{props.data.designation}</Header.Subheader>                                                                                                                                                                                                       
+      </Header.Content>
+    </Header>
+  </Table.Cell>
+  <Table.Cell>
+    <Icon color='green' name='pencil alternate ' link id={props.data.id} onClick={props.onEdit} />                                                                                                                                                                                                        <span>&nbsp;&nbsp;</span>
+    <Icon color='red' name='trash' link id={props.data.id} onClick={props.onDelete} />
+  </Table.Cell>
+
+</Table.Row>
